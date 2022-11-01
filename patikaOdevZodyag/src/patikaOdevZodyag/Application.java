4173847b07241c90ab10cb9c65024fb7f4ffa93c
Eray Nacarlı <69572868+eraynaccarli@users.noreply.github.com>
2022-11-01 09:33:15 +0300

    Add files via upload

---
+++ patikaOdevZodyag/src/patikaOdevZodyag/Application.java
@@ -0,0 +1,58 @@
+package patikaOdevZodyag;
+
+import java.util.Scanner;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+	
+		System.out.println("Enter your birth year: ");
+		int birthYearInput = sc.nextInt();
+		
+		switch(birthYearInput % 12) {
+		
+		case 0: System.out.println("Your Chinese Zodiac Sign: Monkey");
+				break;
+				
+		case 1: System.out.println("Your Chinese Zodiac Sign: Rooster");
+				break;
+				
+		case 2: System.out.println("Your Chinese Zodiac Sign: Dog");
+				break;
+
+		case 3: System.out.println("Your Chinese Zodiac Sign: Pig");
+				break;
+
+		case 4: System.out.println("Your Chinese Zodiac Sign: Mouse");
+				break;
+
+		case 5: System.out.println("Your Chinese Zodiac Sign: Ox");
+				break;
+		
+		case 6: System.out.println("Your Chinese Zodiac Sign: Tiger");
+				break;
+		
+		case 7: System.out.println("Your Chinese Zodiac Sign: Rabbit");
+				break;
+		
+		case 8: System.out.println("Your Chinese Zodiac Sign: Dragon");
+				break;
+
+		case 9: System.out.println("Your Chinese Zodiac Sign: Snake");
+				break;
+
+		case 10: System.out.println("Your Chinese Zodiac Sign: Horse");
+				break;
+		
+		case 11: System.out.println("Your Chinese Zodiac Sign: Sheep");
+				break;
+		
+		default: System.out.println("Please enter invalid value.");
+		
+		}
+	
+	}
+
+}
